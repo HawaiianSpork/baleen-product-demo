dcfbcd529d4bd35eedf890eea60e2ebec5cf6935
Michael R. Maletich <mmaletich@shoprunner.com>
2018-06-11 14:40:12 -0500

    Fix that data trace is no longer an array.

--- src/main/kotlin/com/shoprunner/baleenproductdemo/csv/DataValidationSerializer.kt
+++ src/main/kotlin/com/shoprunner/baleenproductdemo/csv/DataValidationSerializer.kt
@@ -3,12 +3,19 @@ package com.shoprunner.baleenproductdemo.csv
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.shoprunner.baleen.DataTrace
 import com.shoprunner.baleen.ValidationError
 import com.shoprunner.baleen.ValidationSuccess
 import org.springframework.boot.jackson.JsonComponent
 
 @JsonComponent
 class DataValidationSerializer : StdSerializer<DataWithValidation>(DataWithValidation::class.java) {
+
+    // Work arround because Data Trace doesn't currently return array
+    fun dataTraceToArray(dataTrace: DataTrace) : List<String> {
+        val s = dataTrace.toString()
+        return s.substring(1, s.length - 1).split(",")
+    }
 
     override fun serialize(pair: DataWithValidation?, gen: JsonGenerator?, provider: SerializerProvider?) {
         pair ?: return
@@ -17,13 +24,13 @@ class DataValidationSerializer : StdSerializer<DataWithValidation>(DataWithValid
         when (value) {
             is ValidationSuccess -> {
                 gen?.writeObjectField("type", "Success")
-                gen?.writeObjectField("dataTrace", value.dataTrace)
+                gen?.writeObjectField("dataTrace", dataTraceToArray(value.dataTrace))
                 gen?.writeObjectField("value", value.value)
                 gen?.writeObjectField("data", data)
             }
             is ValidationError -> {
                 gen?.writeObjectField("type", "Error")
-                gen?.writeObjectField("dataTrace", value.dataTrace)
+                gen?.writeObjectField("dataTrace", dataTraceToArray(value.dataTrace))
                 gen?.writeObjectField("value", value.value)
                 gen?.writeObjectField("message", value.message)
                 gen?.writeObjectField("data", data)
